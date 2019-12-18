7c7d3443dc0a736432e17b3d155ad4d1617e4b01
Renan <renanxpress@gmail.com>
2019-12-18 09:33:41 -0300

    Correção de bug,  atualização do arq. requirements.txt

--- Blog/views.py
+++ Blog/views.py
@@ -6,14 +6,14 @@ from .forms import PostForm
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'Blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request,pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
-    if request.method == 'Post':
+    if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
